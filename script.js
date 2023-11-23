e93e311d2e92502afeaf403d919ffab4cae3aed6
Duda Elbakidze <147228294+duda-el@users.noreply.github.com>
2023-11-23 23:36:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//navBar
+const menuOpen = document.querySelector(".men");
+const menuClose = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
