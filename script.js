20d8b23812fab2f64ecece387c4ca5896a4dc47f
Duda Elbakidze <147228294+duda-el@users.noreply.github.com>
2023-11-24 03:53:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,46 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+ScrollReveal().reveal('.hero', { delay: 50 });
 ScrollReveal().reveal('.mainSec', { delay: 300 });
 ScrollReveal().reveal('header', { delay: 300 });
 ScrollReveal().reveal('.scroll', { delay: 300 });
-ScrollReveal().reveal('.hero', { delay: 50 });
+ScrollReveal().reveal('.leftContainer', { delay: 1000 });
+ScrollReveal().reveal('.rightContainer', { delay: 2000 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const prevButton = document.getElementById("prev");
+  const nextButton = document.getElementById("next");
+  const img1 = document.querySelector(".item");
+  const img2 = document.querySelector(".item1");
+  const img3 = document.querySelector(".item2");
+
+  let currentIndex = 1; // 
+
+  prevButton.addEventListener("click", function() {
+    if (currentIndex === 2) {
+      img2.style.display = "none";
+      img1.style.display = "block";
+      currentIndex = 1;
+    } else if (currentIndex === 3) {
+      img3.style.display = "none";
+      img2.style.display = "block";
+      currentIndex = 2;
+    }
+  });
+
+  nextButton.addEventListener("click", function() {
+    if (currentIndex === 1) {
+      img1.style.display = "none";
+      img2.style.display = "block";
+      currentIndex = 2;
+    } else if (currentIndex === 2) {
+      img2.style.display = "none";
+      img3.style.display = "block";
+      currentIndex = 3;
+    }
+  });
+});
+
+
