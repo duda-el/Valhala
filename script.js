b1401097654fe099a58dd7024259cb267706fd7b
Duda Elbakidze <147228294+duda-el@users.noreply.github.com>
2023-11-24 00:49:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollDownLink = document.getElementById('scrollDownLink');
+  const aboutValhalaSection = document.getElementById('aboutValhala');
+
+  if (scrollDownLink && aboutValhalaSection) {
+    scrollDownLink.addEventListener('click', function() {
+      aboutValhalaSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+});
+
+ScrollReveal().reveal('.mainSec', { delay: 300 });
+ScrollReveal().reveal('header', { delay: 300 });
+ScrollReveal().reveal('.scroll', { delay: 300 });
+ScrollReveal().reveal('.hero', { delay: 50 });
